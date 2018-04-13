1c4b99e8169cd7ae3109fd90a157831929be1e7f
Hynek Zemanec <zemanech@yahoo.com>
2018-04-13 16:44:29 +0200

    death star init attributes, implementation of HeroShip init attributes

--- src/Ships/DeathStar.java
+++ src/Ships/DeathStar.java
@@ -5,4 +5,19 @@ package Ships;
  * @project StarWars
  */
 public class DeathStar {
+    private int HP;
+    /*
+    The Deathstar starts with 30 Health Points
+     */
+    public DeathStar(){
+        this.HP = 30;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
 }
